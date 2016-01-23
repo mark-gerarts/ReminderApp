e47cc348adccb996d646b1111d68e5b9be5fa02f
mark-gerarts <mark.gerarts@gmail.com>
2016-01-23 16:35:40 +0100

    datetimepicker cont.

--- public/js/datetimetest.vue.js
+++ public/js/datetimetest.vue.js
@@ -2,7 +2,7 @@ var dateTimePicker = Vue.extend({
     template: '#datetimepicker-template',
     props: ['name'],
     ready: function() {
-        console.log(this.labels)
+        this.activeMonth = this.generateMonth(this.currentDate.getFullYear(), this.currentDate.getMonth());
     },
     data: function() {
         return {
@@ -11,11 +11,44 @@ var dateTimePicker = Vue.extend({
                 months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
             },
             daysInMonth: [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
-            currentDate: new Date()
+            currentDate: new Date(),
+            activeMonth: {
+                year: '',
+                month: 0,
+                days: []
+            }
         }
     },
     methods: {
-        generateMonth: function() {
+        generateMonth: function(year, month) {
+            var myDate = new Date(year, month);
+            var startDay = myDate.getDay() - 1;
+            var monthObj = {};
+            monthObj.year = year;
+            monthObj.month = month;
+            monthObj.days = [];
+            
+            var tempDays = [];
+            for(var i=0; i<this.daysInMonth[month]; i++) {
+                if(i<startDay) {
+                    tempDays.push('');
+                } else {
+                    tempDays.push(i - startDay + 1);
+                }
+            }
+            
+            for(var i=0; i<tempDays.length; i += 7) {
+                monthObj.days.push(tempDays.slice(i, i+7));
+            }
+            
+            return monthObj;
+        },
+        
+        nextMonth: function() {
+            
+        },
+        
+        previousMonth: function() {
             
         }
     }
--- resources/views/dashboard/history.blade.php
+++ resources/views/dashboard/history.blade.php
@@ -22,65 +22,23 @@
                 <table class="datetime-table">
                     <thead>
                         <tr>
-                            <th><i class="fa fa-caret-left"></i></th>
-                            <th colspan="5">January 2016</th>
-                            <th><i class="fa fa-caret-right"></i></th>
+                            <th @click="previousMonth">
+                                <i class="fa fa-caret-left"></i>
+                            </th>
+                            <th colspan="5">
+                                @{{ labels.months[activeMonth.month] }} @{{ activeMonth.year }}
+                            </th>
+                            <th @click="nextMonth">
+                                <i class="fa fa-caret-right"></i>
+                            </th>
                         </tr>
                         <tr>
-                            <th>Mon</th>
-                            <th>Tue</th>
-                            <th>Wed</th>
-                            <th>Thu</th>
-                            <th>Fri</th>
-                            <th>Sat</th>
-                            <th>Sun</th>
+                            <th v-for="dayLabel in labels.days">@{{ dayLabel }}</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td>1</td>
-                            <td>2</td>
-                            <td>3</td>
-                            <td>4</td>
-                        </tr>
-                        <tr>
-                            <td>5</td>
-                            <td>6</td>
-                            <td>7</td>
-                            <td>8</td>
-                            <td>9</td>
-                            <td>10</td>
-                            <td>11</td>
-                        </tr>
-                        <tr>
-                            <td>12</td>
-                            <td>13</td>
-                            <td>14</td>
-                            <td>15</td>
-                            <td>16</td>
-                            <td>17</td>
-                            <td>18</td>
-                        </tr>
-                        <tr>
-                            <td>19</td>
-                            <td>20</td>
-                            <td>21</td>
-                            <td>22</td>
-                            <td>23</td>
-                            <td>24</td>
-                            <td>25</td>
-                        </tr>
-                        <tr>
-                            <td>26</td>
-                            <td>27</td>
-                            <td>28</td>
-                            <td>29</td>
-                            <td>30</td>
-                            <td>31</td>
-                            <td></td>
+                        <tr v-for="week in activeMonth.days">
+                            <td v-for="dayNumber in week" track-by="$index" :class="dayNumber ? 'table-day' : ''">@{{ dayNumber }}</td>
                         </tr>
                     </tbody>
                 </table>
